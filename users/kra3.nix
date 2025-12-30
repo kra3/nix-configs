e77e0e10a0d757df174c9dcf0fa155ed3cf70251
Arun Karunagath <48812+kra3@users.noreply.github.com>
2025-12-30 23:11:35 +0100

    set-up ssh

--- users/kra3.nix
+++ users/kra3.nix
@@ -1,9 +1,16 @@
 { config, ... }:
 {
   users.users = {
+    root.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMY4lTahzgn3hOIq3edXBPzg2XdJlcYUBIdWm0BD2HkP sutala root"
+    ];
+
     kra3 = {
       isNormalUser = true;
       extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpvhVfQVKDNfVyl4GJux/lfzjkm683EW4MAESX/JKQA sutala kra3"
+      ];
     };
   };
 }

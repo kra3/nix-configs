b47eb34bac35c327391044b53ddbea36601c023b
Arun Karunagath <48812+kra3@users.noreply.github.com>
2025-12-30 19:51:11 +0100

    create disko partition schema for sutala host machine

--- hosts/sutala/configuration.nix
+++ hosts/sutala/configuration.nix
@@ -2,11 +2,18 @@
 {
   imports = [
     # ./hardware-configuration.nix
+    ./disko.nix
     ../../modules/nix.nix
     ../../users/kra3.nix
   ];
 
   networking.hostName = "sutala";
+  networking.hostId = "d2a81622";
+
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.extraModprobeConfig = ''
+    options zfs zfs_arc_max=3338665984
+  '';
 
   home-manager.users.kra3 = import ../../home/kra3.nix;
 

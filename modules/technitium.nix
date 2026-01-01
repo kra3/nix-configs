29c3f3f5a6d364d0ba8ba5b33f5bf934fb371e73
Arun Karunagath <48812+kra3@users.noreply.github.com>
2026-01-01 22:59:25 +0100

    technitium: dns + adblock

--- modules/technitium.nix
+++ modules/technitium.nix
@@ -1,4 +1,24 @@
+{ lib, ... }:
 {
+  users.groups.technitium = {};
+  users.users.technitium = {
+    isSystemUser = true;
+    group = "technitium";
+    home = "/var/lib/technitium-dns-server";
+    createHome = false;
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/technitium-dns-server 0750 technitium technitium - -"
+    "Z /var/lib/technitium-dns-server - technitium technitium - -"
+  ];
+
+  systemd.services.technitium-dns-server.serviceConfig = {
+    DynamicUser = lib.mkForce false;
+    User = "technitium";
+    Group = "technitium";
+  };
+
   services.technitium-dns-server = {
     enable = true;
     openFirewall = false;

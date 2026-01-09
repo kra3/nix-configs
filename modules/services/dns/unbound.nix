74a2377b10d2dd2e1900e621cf2d289d7eab85bf
Arun Karunagath (kra3) <48812+kra3@users.noreply.github.com>
2026-01-09 17:47:31 +0100

    harden unbound + better protection upstream dns

--- modules/services/dns/unbound.nix
+++ modules/services/dns/unbound.nix
@@ -11,11 +11,18 @@ in
         access-control = [ "127.0.0.0/8 allow" ];
         port = 5335;
         do-daemonize = false;
+        prefetch = "yes";
+        edns-buffer-size = 1232; # why ? 4096 is default, 1480 for fragmentation (seen as timeouts), 512 mtu issues
         harden-glue = "yes";
         harden-dnssec-stripped = "yes";
         use-caps-for-id = "yes";
-        prefetch = "yes";
-        edns-buffer-size = 1232;
+        hide-identity = "yes";
+        hide-version = "yes";
+        minimal-responses = "yes";
+        deny-any = "yes";
+        harden-referral-path = "yes";
+        harden-algo-downgrade = "yes";
+        do-not-query-localhost = "yes";
 
         local-zone = [
           ''"karunagath.in." redirect''
@@ -34,8 +41,11 @@ in
           name = ".";
           forward-tls-upstream = true;
           forward-addr = [
-            "1.1.1.1@853#cloudflare-dns.com"
-            "9.9.9.9@853#dns.quad9.net"
+            # "1.1.1.1@853#cloudflare-dns.com"
+            # "1.1.1.1@853#security.cloudflare-dns.com"
+            "1.1.1.1@853#family.cloudflare-dns.com"
+            # "9.9.9.9@853#dns.quad9.net"
+            "9.9.9.11@853#dns11.quad9.net"
           ];
         }
       ];

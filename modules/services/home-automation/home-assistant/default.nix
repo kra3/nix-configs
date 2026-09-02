08c9bb250edd6c70f0ef3f98f6ddab1999dc689c
Arun Karunagath <48812+kra3@users.noreply.github.com>
2026-09-02 22:05:44 +0200

    Update ghcr.io/home-assistant/home-assistant Docker tag to v2026.9.0 (#88)
    Co-authored-by: Renovate <renovate@whitesourcesoftware.com>

--- modules/services/home-automation/home-assistant/default.nix
+++ modules/services/home-automation/home-assistant/default.nix
@@ -3,7 +3,7 @@
   {
     virtualisation.quadlet.containers.home-assistant = {
       containerConfig = {
-        image = "ghcr.io/home-assistant/home-assistant:2026.8.2";
+        image = "ghcr.io/home-assistant/home-assistant:2026.9.0";
         publishPorts = [ "127.0.0.1:8123:8123" ];
         logDriver = "journald";
         environments = {

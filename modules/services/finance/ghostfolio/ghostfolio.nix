a14d6afa07992d0d81cfee2cab531417bba2b000
Arun Karunagath <48812+kra3@users.noreply.github.com>
2026-09-15 20:44:08 +0200

    Update docker.io/ghostfolio/ghostfolio Docker tag to v3.70.1 (#260)
    Co-authored-by: Renovate <renovate@whitesourcesoftware.com>

--- modules/services/finance/ghostfolio/ghostfolio.nix
+++ modules/services/finance/ghostfolio/ghostfolio.nix
@@ -4,7 +4,7 @@
     {
       virtualisation.quadlet.containers.ghostfolio = {
         containerConfig = {
-          image = "docker.io/ghostfolio/ghostfolio:3.70.0";
+          image = "docker.io/ghostfolio/ghostfolio:3.70.1";
           healthCmd = "curl -sf http://localhost:3333/api/v1/health";
           healthOnFailure = "none";
           healthInterval = "30s";
